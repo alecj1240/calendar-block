a935286bf448bed4486d297e6fd820aa7ac9111c
Alec Jones <alecaej2002@gmail.com>
2020-07-05 11:05:14 -0700

    updated look of login page

--- frontend/loginScreen.js
+++ frontend/loginScreen.js
@@ -9,25 +9,69 @@ export default function LoginScreen({userId, login}) {
   return (
     <div className="centerPage">
       <Box
+        as="header"
         display="flex"
         alignItems="center"
         justifyContent="center"
         border="default"
         backgroundColor="white"
+        padding={2}
+        overflow="hidden"
+        marginBottom={1}
+      >
+      <Text size="xlarge">Welcome to The Google Calendar Block</Text>
+      </Box>
+      <Box
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        backgroundColor="white"
         padding={0}
         overflow="hidden"
+        margin={1}
       >
-        <Text size="xlarge">Welcome to The Google Calendar Block</Text>
+        <Text>To start using this block, you must first login with Google</Text>
+      </Box>
+      <Box
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        backgroundColor="white"
+        padding={0}
+        overflow="hidden"
+        margin={1}
+      >
         <Iframe 
-            url={"https://google-oauth.now.sh?user_id=" + userId}
-            width="100px"
-            height="50px"
-            display="initial"
-            position="relative"
-            frameBorder="0"
+          url={"https://google-oauth.now.sh?user_id=" + userId}
+          width="100px"
+          height="50px"
+          display="initial"
+          position="relative"
+          frameBorder="0"
         />
       </Box>
-      <Button onClick={() => handleChange(null)}>I have logged in</Button>
+      <Box
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        backgroundColor="white"
+        padding={0}
+        overflow="hidden"
+        margin={1}
+      >
+        <Text>Once you have logged in, click the button below to start using this block</Text>
+      </Box>
+      <Box
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        backgroundColor="white"
+        padding={0}
+        overflow="hidden"
+        margin={1}
+      >
+        <Button onClick={() => handleChange(null)}>See My Events</Button>
+      </Box>
     </div>
   );
 }
